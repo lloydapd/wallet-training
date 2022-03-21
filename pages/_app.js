d1a63a460e9fe41d00683603fcc012e18998f53b
Lloyd <lloyd.appdeture@gmail.com>
2022-03-21 15:47:20 +0800

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import { UseWalletProvider } from 'use-wallet'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UseWalletProvider
+      connectors={{
+        injected: {
+          chainId: [1, 56],
+        },
+        walletconnect: {
+          chainId: 1,
+          rpc: { 1: 'https://mainnet.eth.aragon.network/', 137: "https://matic-mainnet.chainstacklabs.com" }
+        },
+        walletlink: {
+          chainId: 1,
+          url: 'https://mainnet.eth.aragon.network/',
+        },
+      }}
+      >
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </UseWalletProvider>
+  )
 }
 
 export default MyApp
